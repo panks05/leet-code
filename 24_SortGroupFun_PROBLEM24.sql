e04acb5cd7c560e7a83d2489d0336f33075f19a3
Pankti Turakhiya <35331687+panks05@users.noreply.github.com>
2026-04-13 14:36:04 -0700

    Update 24_SortGroupFun_PROBLEM24.sql

--- 24_SortGroupFun_PROBLEM24.sql
+++ 24_SortGroupFun_PROBLEM24.sql
@@ -59,5 +59,6 @@ SELECT
 activity_date as day,
 COUNT(DISTINCT user_id) as active_users
 FROM Activity
-WHERE activity_date >= DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND activity_date <= '2019-07-27'
+WHERE activity_date BETWEEN DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND '2019-07-27'
 GROUP BY activity_date
+
